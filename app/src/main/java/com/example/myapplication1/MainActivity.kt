eff86a6540fb2a79b4f289ab2de92df8d2d4d002
IshanChhangani <f20200230@hyderabad.bits-pilani.ac.in>
2023-11-15 18:44:05 +0530

    second commit

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.example.myapplication1
+
+import android.graphics.Color
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+
+        val showPopupButton: Button = binding.showPopupButton
+        val blankScreenButton: Button = binding.blankScreenButton
+        val restoreScreenButton: Button = binding.restoreScreenButton
+        val timerEditText: EditText = binding.timerEditText
+        val startCounterButton: Button = binding.startCounterButton
+
+        showPopupButton.setOnClickListener {
+            showPopup()
+        }
+
+        blankScreenButton.setOnClickListener {
+            startCounter()
+        }
+
+        restoreScreenButton.setOnClickListener {
+            restoreScreen()
+        }
+
+        startCounterButton.setOnClickListener {
+            val duration = timerEditText.text.toString().toLong()
+            startCounterWithCustomTime(duration)
+        }
+    }
+
+    private fun showPopup() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Popup Title")
+            .setMessage("This is the content of the popup.")
+            .setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun startCounter() {
+        val duration = 10L // Default duration if no value entered
+        startCounterWithCustomTime(duration)
+    }
+
+    private fun restoreScreen() {
+        val rootView: View = window.decorView.rootView
+        rootView.setBackgroundColor(Color.WHITE)
+    }
+
+    private fun startCounterWithCustomTime(duration: Long) {
+        val counterTextView: TextView = binding.counterTextView
+        counter = 0
+
+        val countDownTimer = object : CountDownTimer(duration * 1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                counter++
+                val counterText = resources.getString(R.string.counter_text, counter)
+                counterTextView.text = counterText
+            }
+
+            override fun onFinish() {
+                val counterText = resources.getString(R.string.counter_text, counter)
+                counterTextView.text = counterText
+                blankScreen()
+            }
+        }
+
+        countDownTimer.start()
+    }
+
+    private fun blankScreen() {
+        val rootView: View = window.decorView.rootView
+        rootView.setBackgroundColor(Color.BLACK)
+    }
+}
